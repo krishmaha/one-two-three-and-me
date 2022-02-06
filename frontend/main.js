be6e58b44c2567a4f88571e5bc41ae5b6c263a2e
Rishav Chatterjee <chat.rishav7@gmail.com>
2022-02-06 04:45:05 +0000

    operations

--- frontend/main.js
+++ frontend/main.js
@@ -1,52 +1,69 @@
 const el = (id) => document.getElementById(id);
 let canvas = el("canvas");
 let context = canvas.getContext("2d");
+let checkAnswer = 0;
 
 el("check").onclick = async function () {
   const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 
   const getGrayScaleValue = (x, y) => {
     const red = y * (canvas.width * 4) + x * 4;
-    const redCoord = imageData.data[red]
-    const blueCoord = imageData.data[red + 1]
-    const greenCoord = imageData.data[red + 2]
+    const redCoord = imageData.data[red];
+    const blueCoord = imageData.data[red + 1];
+    const greenCoord = imageData.data[red + 2];
     return (redCoord + blueCoord + greenCoord) / 3;
-  }
-  
-  let grayScaleImage = []
+  };
+
+  let grayScaleImage = [];
   for (let i = 0; i < canvas.height; i++) {
     for (let j = 0; j < canvas.width; j++) {
-      grayScaleImage.push(getGrayScaleValue(j, i))
+      grayScaleImage.push(getGrayScaleValue(j, i));
     }
   }
 
-  console.log(JSON.stringify(grayScaleImage))
+  console.log(JSON.stringify(grayScaleImage));
 
   const response = await fetch("http://127.0.0.1:8000/model/predict/", {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Origin': 'http://127.0.0.1:8887/'
+      Origin: "http://127.0.0.1:8887/",
     },
-    body: JSON.stringify({'image' : grayScaleImage})
-  })
+    body: JSON.stringify({ image: grayScaleImage }),
+  });
 
   const data = await response.json();
-  
+
   console.log(data.predicted);
   console.log(data.score);
-
 };
 
-el("shuffle").onclick = function () {
-  let newNumber = Math.floor(Math.random() * 10);
-  el("number").textContent = newNumber;
+const newNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+el("plus").onclick = function () {
+  let num1 = newNumber(0, 9);
+  let num2 = newNumber(0, 10 - num1);
+  el("number").textContent = num1 + " + " + num2;
+  checkAnswer = num1 + num2;
+};
+
+el("minus").onclick = function () {
+  let num1 = newNumber(2, 10);
+  let num2 = newNumber(1, num1);
+  el("number").textContent = num1 + " - " + num2;
+  checkAnswer = num1 - num2;
+};
+
+el("newnum").onclick = function () {
+  let num = newNumber(0, 10);
+  el("number").textContent = num;
+  checkAnswer = num;
 };
 
 el("clear").onclick = function () {
   context.clearRect(0, 0, canvas.width, canvas.height);
 };
-
-el("settings").onclick = function () {};
 
 const modal = document.querySelector(".modal");
 const trigger = document.querySelector(".trigger");
